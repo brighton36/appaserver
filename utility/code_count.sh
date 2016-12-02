4c4d74d6e7a2fb38444d21039937c0f796e59f03
Tim Riley <timriley@appahost.com>
2016-12-02 15:33:27 -0800

    new file code_count.sh

---
+++ utility/code_count.sh
@@ -0,0 +1,22 @@
+:
+
+/bin/echo -n "File count: "
+(
+for file in `find . -name '*.c' -o -name '*.h' -o -name makefile -o -name '*.js'`
+do
+	echo $file
+done
+) | wc -l | commas_in_long.e
+
+/bin/echo -n "Line count: "
+(
+for file in `find . -name '*.c' -o -name '*.h' -o -name makefile -o -name '*.js'`
+do
+	cat $file
+done
+# ------------------------------
+# Skip empty lines and comments.
+# ------------------------------
+) | grep -v '^$' | grep -v '^\/\*' | wc -l | commas_in_long.e
+
+exit 0

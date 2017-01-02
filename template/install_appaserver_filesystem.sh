993f2520a1f77ac4c5d1df819deec28bf875b46e
Tim Riley <timriley@appahost.com>
2017-01-01 20:35:51 -0800

    installation scripts

--- template/install_appaserver_filesystem.sh
+++ template/install_appaserver_filesystem.sh
@@ -4,138 +4,321 @@
 
 if [ "$#" -ne 1 ]
 then
-	echo "Usage: $0 group" 1>&2
+	echo "Usage: $0 execute|noexecute" 1>&2
 	exit 1
 fi
 
-group=$1
+execute=$1
+
+profile_file="/etc/profile"
+appaserver_config_file="/etc/appaserver.config"
 
 integrity_check ()
 {
-	if [ "$USER" != "root" ]
-	then
-		echo "You must be root to run this." 1>&2
-		echo "Try: sudo sh" 1>&2
-		echo "Then:. /etc/profile" 1>&2
-		exit 1
-	fi
+	group=$1
+	cgi_home=$2
 
-	if [ ! -d /usr/lib/cgi-bin ]
+#	if [ "$USER" != "root" ]
+#	then
+#		echo "Error: You must be root to run this." 1>&2
+#		echo "Try: sudo sh" 1>&2
+#		echo "Then:. /etc/profile" 1>&2
+#		exit 1
+#	fi
+
+	if [ ! -d "${cgi_home}" ]
 	then
-		echo "Mysql doesn't seem to be installed." 1>&2
-		echo "Try: sudo apt-get -y install mysql-server" 1>&2
+		echo "Error: ${cgi_home} is not found in $appaserver_config_file" 1>&2
 		exit 1
 	fi
 
 	if [ "$DOCUMENT_ROOT" = "" ]
 	then
-		echo "DOCUMENT_ROOT must be set." 1>&2
-		echo "Try: . /etc/profile" 1>&2
+		echo "Error: DOCUMENT_ROOT must be set." 1>&2
+		echo "Try: . ${profile_file}" 1>&2
 		exit 1
 	fi
 
 	if [ "$APPASERVER_HOME" = "" ]
 	then
-		echo "APPASERVER_HOME must be set." 1>&2
-		echo "Try: . /etc/profile" 1>&2
+		echo "Error: APPASERVER_HOME must be set." 1>&2
+		echo "Try: . ${profile_file}" 1>&2
 		exit 1
 	fi
 
+	if [ "`umask`" != "0002" ]
+	then
+		echo "Error: umask needs to be set to 0002." 1>&2
+		echo "Try: . ${profile_file}" 1>&2
+		exit 1
+	fi
+
+SUDO_USER=timriley
+
+	if [ "$SUDO_USER" = "" ]
+	then
+		echo "Error: Cannot determine your regular user name." 1>&2
+		echo "Try: sudo sh" 1>&2
+		echo "Then:. /etc/profile" 1>&2
+		exit 1
+	fi
+
+	group_exists=`grep "^${group}" /etc/group`
+
+	if [ "$group_exists" = "" ]
+	then
+		echo "Error: Group $group doesn't exist in /etc/group" 1>&2
+		exit 1
+	fi
+
+	user_exists=`grep "^${group}" /etc/group | grep $SUDO_USER`
+
+	if [ "$user_exists" = "" ]
+	then
+		echo "Error: User $SUDO_USER doesn't belong to group $group in /etc/group" 1>&2
+		exit 1
+	fi
 }
 
-append_etc_profile ()
-{
-	echo "" >> /etc/profile
-	echo "# Added by $0" >> /etc/profile
-	echo "# ======================================" >> /etc/profile
-	echo "umask 0002" >> /etc/profile
-	echo "export APPASERVER_HOME=/var/appaserver" >> /etc/profile
-	echo "export CGI_HOME=/usr/lib/cgi-bin" >> /etc/profile
-	echo "export APPASERVER_UNUSED_PARAMETER_FLAG=-Wunused-parameter" >> /etc/profile
-	echo "export DOCUMENT_ROOT=/var/www" >> /etc/profile
-	echo 'export PATH=$PATH:$APPASERVER_HOME/utility:$APPASERVER_HOME/src_appaserver:.' >> /etc/profile
-	echo "#export APPASERVER_LINK_FLAGS=-s" >> /etc/profile
-	echo "set -o vi" >> /etc/profile
-}
-
-change_permissions_cgi_bin ()
+change_permissions_cgi_home ()
 {
 	group=$1
-	cd /usr/lib
-	chgrp $group cgi-bin
-	chmod g+rwxs cgi-bin
-	chmod o-rx cgi-bin
+	cgi_home=$2
+	execute=$3
+
+	if [ "$execute" = "execute" ]
+	then
+		chgrp $group ${cgi_home}
+		chmod g+rwxs ${cgi_home}
+		chmod o-rwx ${cgi_home}
+	else
+		echo "chgrp $group ${cgi_home}"
+		echo "chmod g+rwxs ${cgi_home}"
+		echo "chmod o-rwx ${cgi_home}"
+	fi
 }
 
-change_permissions_var_www ()
+create_document_root_template ()
+{
+	execute=$1
+
+	template_directory="${DOCUMENT_ROOT}/template"
+
+	if [ "$execute" = "execute" ]
+	then
+		mkdir $template_directory
+		mkdir ${template_directory}/appaserver
+		mkdir ${template_directory}/images
+	else
+		echo "mkdir $template_directory"
+		echo "mkdir ${template_directory}/appaserver"
+		echo "mkdir ${template_directory}/images"
+	fi
+}
+
+copy_document_root_template ()
+{
+	execute=$1
+
+	template_directory="${DOCUMENT_ROOT}/template"
+	logo_filename="images/logo_appaserver.jpg"
+
+	if [ "$execute" = "execute" ]
+	then
+		cp index.php $template_directory
+		ln ${template_directory}/index.php ${template_directory}/appaserver/index.php
+		cp style.css $template_directory
+		ln ${template_directory}/style.css ${template_directory}/appaserver/style.css
+		cp $logo_filename ${template_directory}/images
+	else
+		echo "cp index.php $template_directory"
+		echo "ln ${template_directory}/index.php ${template_directory}/appaserver/index.php"
+		echo "cp style.css $template_directory"
+		echo "ln ${template_directory}/style.css ${template_directory}/appaserver/style.css"
+		echo "cp $logo_filename ${template_directory}/images"
+	fi
+}
+
+link_document_root_src_template ()
+{
+	execute=$1
+
+	src_template_directory="${APPASERVER_HOME}/src_template"
+
+	if [ "$execute" = "execute" ]
+	then
+		ln -s $src_template_directory $DOCUMENT_ROOT
+	else
+		echo "ln -s $src_template_directory $DOCUMENT_ROOT"
+	fi
+}
+
+change_permissions_document_root ()
 {
 	group=$1
-	cd $DOCUMENT_ROOT
-	chgrp $group .
-	chmod g+rwxs .
-	chmod o-rwx .
+	execute=$2
+
+	if [ "$execute" = "execute" ]
+	then
+		chgrp $group $DOCUMENT_ROOT
+		chmod g+rwxs $DOCUMENT_ROOT
+		chmod o-rwx $DOCUMENT_ROOT
+	else
+		echo "chgrp $group $DOCUMENT_ROOT"
+		echo "chmod g+rwxs $DOCUMENT_ROOT"
+		echo "chmod o-rwx $DOCUMENT_ROOT"
+	fi
 }
 
 create_appaserver_error_directory ()
 {
 	group=$1
-	cd /var/log
-	mkdir appaserver
-	chgrp $group appaserver
-	chmod g+rwxs appaserver
-	chmod o-rwx appaserver
+	appaserver_error=$2
+	execute=$3
+
+	if [ "$execute" = "execute" ]
+	then
+		mkdir $appaserver_error
+		chgrp $group $appaserver_error
+		chmod g+rwxs $appaserver_error
+		chmod o-rwx $appaserver_error
+	else
+		echo "mkdir $appaserver_error"
+		echo "chgrp $group $appaserver_error"
+		echo "chmod g+rwxs $appaserver_error"
+		echo "chmod o-rwx $appaserver_error"
+	fi
 }
 
-create_var_appaserver ()
+protect_appaserver_home ()
 {
 	group=$1
-	cd /var
-	mkdir appaserver
-	chgrp $group appaserver
-	chmod g+rwxs appaserver
-	chmod o-w appaserver
+	execute=$2
+
+	if [ "$execute" = "execute" ]
+	then
+		chgrp $group $APPASERVER_HOME
+		chmod g+rwxs $APPASERVER_HOME
+		chmod o-w $APPASERVER_HOME
+	else
+		echo "chgrp $group $APPASERVER_HOME"
+		echo "chmod g+rwxs $APPASERVER_HOME"
+		echo "chmod o-w $APPASERVER_HOME"
+	fi
 }
 
 create_old_appaserver_error_file ()
 {
-	cd /var/log/appaserver
-	/bin/echo > appaserver.err
-	chmod o-r appaserver.err
+	group=$1
+	execute=$2
+
+	old_appaserver_error_file="/var/log/appaserver.err"
+
+	if [ "$execute" = "execute" ]
+	then
+		touch $old_appaserver_error_file
+		chgrp $group $old_appaserver_error_file
+		chmod g+rw $old_appaserver_error_file
+		chmod o-r $old_appaserver_error_file
+	else
+		echo "touch $old_appaserver_error_file"
+		echo "chgrp $group $old_appaserver_error_file"
+		echo "chmod g+rw $old_appaserver_error_file"
+		echo "chmod o-rw $old_appaserver_error_file"
+	fi
 }
 
-create_template_error_file ()
-{
-	cd /var/log/appaserver
-	/bin/echo > appaserver_template.err
-	chmod o-r appaserver_template.err
-}
-
-create_appaserver_config ()
+create_error_file_template ()
 {
 	group=$1
-	cd /etc
-	cp $APPASERVER_HOME/appaserver_template.config appaserver.config
-	chgrp $group appaserver.config
-	chmod g+w appaserver.config
-	chmod o-r appaserver.config
+	appaserver_error=$2
+	execute=$3
+
+	template_error_file="${appaserver_error}/appaserver_template.err"
+
+	if [ "$execute" = "execute" ]
+	then
+		touch $template_error_file
+		chgrp $group $template_error_file
+		chmod g+rw $template_error_file
+		chmod o-rw $template_error_file
+	else
+		echo "mkdir $template_error_file"
+		echo "chgrp $group $template_error_file"
+		echo "chmod g+rw $template_error_file"
+		echo "chmod o-rw $template_error_file"
+	fi
 }
 
-create_var_appaserver_data ()
+protect_appaserver_config ()
 {
-	cd $APPASERVER_HOME
-	mkdir data
-	chmod o-rx data
+	group=$1
+	execute=$2
+
+	if [ "$execute" = "execute" ]
+	then
+		chgrp $group $appaserver_config_file
+		chmod g+rw $appaserver_config_file
+		chmod o-rw $appaserver_config_file
+	else
+		echo "chgrp $group $appaserver_config_file"
+		echo "chmod g+rw $appaserver_config_file"
+		echo "chmod o-rw $appaserver_config_file"
+	fi
 }
 
-integrity_check
-append_etc_profile
-change_permissions_cgi_bin $group
-change_permissions_var_www $group
-create_var_appaserver $group
-create_appaserver_error_directory $group
-create_old_appaserver_error_file
-create_template_error_file
-create_appaserver_config $group
-create_var_appaserver_data
+create_appaserver_data ()
+{
+	group=$1
+	appaserver_data=$2
+	execute=$3
+
+	if [ "$execute" = "execute" ]
+	then
+		mkdir $appaserver_data
+		chgrp $group $appaserver_data
+		chmod g+rwxs $appaserver_data
+		chmod o-rwx $appaserver_data
+	else
+		echo "mkdir $appaserver_data"
+		echo "chgrp $group $appaserver_data"
+		echo "chmod g+rwxs $appaserver_data"
+		echo "chmod o-rwx $appaserver_data"
+	fi
+}
+
+label="appaserver_group="
+group=`cat $appaserver_config_file	| \
+	 grep "^${label}"		| \
+	 sed "s/$label//"`
+
+label="cgi_home="
+cgi_home=`cat $appaserver_config_file	| \
+	 	grep "^${label}"	| \
+	 	sed "s/$label//"`
+
+label="appaserver_error_directory="
+appaserver_error=`cat $appaserver_config_file	| \
+	 		grep "^${label}"	| \
+	 		sed "s/$label//"`
+
+label="appaserver_data_directory="
+appaserver_data=`cat $appaserver_config_file	| \
+	 		grep "^${label}"	| \
+	 		sed "s/$label//"`
+
+integrity_check $group $cgi_home
+
+change_permissions_cgi_home $group $cgi_home $execute
+change_permissions_document_root $group $execute
+protect_appaserver_home $group $execute
+create_appaserver_error_directory $group $appaserver_error $execute
+create_old_appaserver_error_file $group $execute
+create_appaserver_data $group $appaserver_data $execute
+protect_appaserver_config $group $execute
+create_error_file_template $group $appaserver_error $execute
+create_document_root_template $execute
+copy_document_root_template $execute
+link_document_root_src_template $execute
 
 exit 0
---
+++ template/install_appaserver_profile.sh
@@ -0,0 +1,93 @@
+:
+# Freely available software. See appaserver.org
+# ---------------------------------------------
+
+if [ "$#" -ne 1 ]
+then
+	echo "Usage: sudo $0 execute|noexecute" 1>&2
+	exit 1
+fi
+
+execute=$1
+
+profile_file="/etc/profile"
+appaserver_config_file="/etc/appaserver.config"
+
+integrity_check ()
+{
+	if [ "$USER" != "root" ]
+	then
+		echo "You must be root to run this." 1>&2
+		exit 1
+	fi
+
+	if [ ! -r $appaserver_config_file ]
+	then
+		echo "Can't read $appaserver_config_file" 1>&2
+		exit 1
+	fi
+}
+
+append_etc_profile_display ()
+{
+	appaserver_home=$1
+	document_root=$2
+
+	echo "Will append the following to ${profile_file}"
+	echo "--------------------------------------------"
+
+	echo "# Added by $0"
+	echo "# =============================================="
+	echo "# See group_centric.txt"
+	echo "umask 0002"
+	echo ""
+	echo "export APPASERVER_HOME=${appaserver_home}"
+	echo "export CGI_HOME=/usr/lib/cgi-bin"
+	echo "export APPASERVER_UNUSED_PARAMETER_FLAG=-Wunused-parameter"
+	echo "export DOCUMENT_ROOT=${document_root}"
+	echo 'export PATH=$PATH:$APPASERVER_HOME/utility:$APPASERVER_HOME/src_appaserver:.'
+	echo "# Uncomment if APPASERVER_HOME and CGI_HOME are on different filesystems"
+	echo "#export APPASERVER_LINK_FLAGS=-s"
+}
+
+append_etc_profile_execute ()
+{
+	appaserver_home=$1
+	document_root=$2
+
+	echo "" >> ${profile_file}
+	echo "# Added by $0" >> ${profile_file}
+	echo "# ==============================================" >> ${profile_file}
+	echo "# See group_centric.txt" >> ${profile_file}
+	echo "umask 0002" >> ${profile_file}
+	echo "" >> ${profile_file}
+	echo "export APPASERVER_HOME=${appaserver_home}" >> ${profile_file}
+	echo "export CGI_HOME=/usr/lib/cgi-bin" >> ${profile_file}
+	echo "export APPASERVER_UNUSED_PARAMETER_FLAG=-Wunused-parameter" >> ${profile_file}
+	echo "export DOCUMENT_ROOT=${document_root}" >> ${profile_file}
+	echo 'export PATH=$PATH:$APPASERVER_HOME/utility:$APPASERVER_HOME/src_appaserver:.' >> ${profile_file}
+	echo "# Uncomment is APPASERVER_HOME and CGI_HOME are on different filesystems" >> ${profile_file}
+	echo "#export APPASERVER_LINK_FLAGS=-s" >> ${profile_file}
+}
+
+integrity_check
+
+label="appaserver_mount_point="
+appaserver_home=`cat $appaserver_config_file	| \
+		 grep "^${label}"		| \
+		 sed "s/$label//"`
+
+
+label="document_root="
+document_root=`cat $appaserver_config_file	| \
+		 grep "^${label}"		| \
+		 sed "s/$label//"`
+
+if [ "$execute" = "execute" ]
+then
+	append_etc_profile_execute $appaserver_home $document_root
+else
+	append_etc_profile_display $appaserver_home $document_root
+fi
+
+exit 0

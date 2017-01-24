8c474bd9bbb5a2977775f5729c508cef9ca4b3fc
Tim Riley <timriley@appahost.com>
2017-01-24 11:41:56 -0800

    enhancing useradd.sh

--- utility/useradd.sh
+++ utility/useradd.sh
@@ -10,12 +10,6 @@ then
 	exit 1
 fi
 
-if [ "$APPASERVER_HOME" = "" ]
-then
-	echo "Error: APPASERVER_HOME not defined." 1>&2
-	exit 1
-fi
-
 username=$1
 
 if [ "$#" -eq 2 ]
@@ -26,34 +20,39 @@ fi
 home_directory=/home/$username
 cgi_directory=$home_directory/cgi-bin/
 
-# Create the username
-# -------------------
-if [ "$group" = "" ]
+# Create the username and default group
+# -------------------------------------
+sudo useradd -m $username
+sudo addgroup $username
+sudo usermod -a -G $username $username
+
+# Assign to group
+# ---------------
+if [ "$group" != "" ]
 then
-	sudo useradd -m $username
-else
-	sudo useradd -m -G $group $username
+	sudo addgroup $group
+	sudo usermod -a -G $group $username
 fi
 
 sudo passwd $username
 
 # Manage the home directory and subdirectories
 # --------------------------------------------
-sudo chmod g+ws $home_directory
+#sudo chmod g+ws $home_directory
 sudo mkdir $home_directory/public_html
+sudo chown ${username}:${username} $home_directory/public_html
 sudo chmod g+ws $home_directory/public_html
-sudo chown $username $home_directory/public_html
 
 sudo mkdir $home_directory/public_html/appaserver
-sudo chown $username $home_directory/public_html/appaserver
+sudo chown ${username}:${username} $home_directory/public_html/appaserver
 sudo chmod g+ws $home_directory/public_html/appaserver
 sudo chmod o-rw $home_directory/public_html/appaserver
 sudo chmod o+x $home_directory/public_html/appaserver
 
 sudo mkdir $cgi_directory
+sudo chown ${username}:${username} $cgi_directory
 sudo chmod g+ws $cgi_directory
 sudo chmod o-rws $cgi_directory
-sudo chown $username $cgi_directory
 
 exit 0
 

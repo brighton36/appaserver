36216769356f2eb231921deb0d86ea033471f997
Tim Riley <timriley@appahost.com>
2017-06-13 10:26:56 -0700

    writing additional species report

--- src_creel/additional_species_report.sh
+++ src_creel/additional_species_report.sh
@@ -1,40 +1,44 @@
 :
-# src_creel/catches_per_day.sh
-# -------------------------------------------
+# $APPASERVER_HOME/src_creel/additional_species_report.sh
+# -------------------------------------------------------
 
 # Input
 # -----
 
 echo "$0" "$*" 1>&2
 
-if [ "$#" -ne 6 ]
+if [ "$#" -ne 9 ]
 then
-	echo "Usage: $0 application process_name begin_date end_date fishing_purpose output_medium" 1>&2
+	echo "Usage: $0 application process_name fishing_purpose begin_date end_date family genus species output_medium" 1>&2
 	exit 1
 fi
 
 application=$(echo $1 | piece.e ':' 0)	# May have appended database
 process_name=$2                       	# Assumed letters_and_underbars
 
-begin_date=$3
-if [ "$begin_date" = "" -o "$begin_date" = "begin_date" ]
-then
-	begin_date="1901-01-01"
-fi
-
-end_date=$4
-if [ "$end_date" = "" -o "$end_date" = "end_date" ]
-then
-	end_date="2099-12-31"
-fi
-
-fishing_purpose=$5
+fishing_purpose=$3
 if [ "$fishing_purpose" = "" -o "$fishing_purpose" = "fishing_purpose" ]
 then
 	fishing_purpose="sport"		# Defaults to sport
 fi
 
-output_medium=$6
+begin_date=$4
+if [ "$begin_date" = "" -o "$begin_date" = "begin_date" ]
+then
+	begin_date="1901-01-01"
+fi
+
+end_date=$5
+if [ "$end_date" = "" -o "$end_date" = "end_date" ]
+then
+	end_date="2099-12-31"
+fi
+
+family=$6
+genus=$7
+species=$8
+
+output_medium=$9
 if [ "$output_medium" = "" -o "$output_medium" = "output_medium" ]
 then
 	output_medium="table"		# Defaults to table
@@ -45,30 +49,25 @@ fi
 document_root=$(get_document_root.e)
 output_file="$document_root/appaserver/$application/data/${process_name}_$$.csv"
 prompt_file="/appaserver/$application/data/${process_name}_$$.csv"
+process_title_half=$(echo $process_name \(${fishing_purpose}\) for ${family}/${genus}							|
+format_initial_capital.e)
+process_title=${process_title_half}/$species
 
-# Variables
-# ---------
-fishing_trips=`get_table_name $application fishing_trips`
-catches=`get_table_name $application catches`
-process_title=$(echo $fishing_purpose $process_name | format_initial_capital.e)
+heading="Fishing Count,Family,Genus,Species"
+justification="right,left,left,left"
 
-# Process
-# -------
-statement="select $fishing_trips.census_date, sum( kept_count + released_count )
-from $catches, $fishing_trips
-where $fishing_trips.census_date between '$begin_date' and '$end_date'
-and $fishing_trips.fishing_purpose = $catches.fishing_purpose
-and $fishing_trips.census_date = $catches.census_date
-and $fishing_trips.interview_location = $catches.interview_location
-and $fishing_trips.interview_number = $catches.interview_number
-and $fishing_trips.fishing_purpose = '$fishing_purpose'
-group by $fishing_trips.census_date;"
-
-heading="Date,Caught"
-justification="left,right"
-
-echo "$heading" > $output_file
-echo "$statement" | sql.e ',' >> $output_file
+echo "#${process_title}" > $output_file
+echo "$heading" >> $output_file
+additional_species	$application		\
+			$fishing_purpose	\
+			$begin_date		\
+			$end_date		\
+			"$family"		\
+			"$genus"		\
+			"$species"		|
+tr '^' ','					|
+tr '|' ','					|
+cat >> $output_file
 
 # Output
 # ------
@@ -87,6 +86,7 @@ elif [ "$output_medium" = "table" ]
 then
 	cat $output_file				|
 	grep -v "$heading"				|
+	grep -v "^#"					|
 	html_table.e "" "$heading" "," "$justification"
 elif [ "$output_medium" = "stdout" ]
 then

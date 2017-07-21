e74e88b39a0c24b1935bda304e20951e66deb019
Tim Riley <timriley@appahost.com>
2017-07-20 20:37:45 -0700

    writing depreciate_fixed_assets.c

--- src_accountancymodel/depreciate_fixed_assets.c
+++ src_accountancymodel/depreciate_fixed_assets.c
@@ -145,6 +145,10 @@ int main( int argc, char **argv )
 					application_name,
 					fund_name,
 					depreciation_date );
+
+				printf(
+				"<h3>Depreciation now posted on %s.</h3>\n",
+				depreciation_date );
 			}
 		}
 	}

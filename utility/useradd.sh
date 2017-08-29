a7214a2ae1928502bc6d490e43edf7740c916c33
Tim Riley <timriley@appahost.com>
2017-08-29 12:49:21 -0700

    streamlining new virtual host

--- utility/useradd.sh
+++ utility/useradd.sh
@@ -17,17 +17,14 @@ then
 	group=$2
 fi
 
-home_directory=/home/$username
-cgi_directory=$home_directory/cgi-bin/
-
 # Create the username and default group
 # -------------------------------------
 sudo useradd -m $username
 sudo addgroup $username
 sudo usermod -a -G $username $username
 
-# Assign to group
-# ---------------
+# Assign to additional group
+# --------------------------
 if [ "$group" != "" ]
 then
 	sudo addgroup $group
@@ -35,24 +32,6 @@ then
 fi
 
 sudo passwd $username
-
-# Manage the home directory and subdirectories
-# --------------------------------------------
-#sudo chmod g+ws $home_directory
-sudo mkdir $home_directory/public_html
-sudo chown ${username}:${username} $home_directory/public_html
-sudo chmod g+ws $home_directory/public_html
-
-sudo mkdir $home_directory/public_html/appaserver
-sudo chown ${username}:${username} $home_directory/public_html/appaserver
-sudo chmod g+ws $home_directory/public_html/appaserver
-sudo chmod o-rw $home_directory/public_html/appaserver
-sudo chmod o+x $home_directory/public_html/appaserver
-
-sudo mkdir $cgi_directory
-sudo chown ${username}:${username} $cgi_directory
-sudo chmod g+ws $cgi_directory
-sudo chmod o-rws $cgi_directory
 
 exit 0
 

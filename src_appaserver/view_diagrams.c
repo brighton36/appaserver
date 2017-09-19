b8233a5836fe20c84fab9cad1494b08bf5472287
Tim Riley <timriley@appahost.com>
2017-09-19 11:06:37 -0700

    allow for multiple source directories

--- src_appaserver/view_diagrams.c
+++ src_appaserver/view_diagrams.c
@@ -34,19 +34,25 @@
 
 /* Prototypes */
 /* ---------- */
+void view_diagrams_generic(
+			char *application_name,
+			char *appaserver_mount_point );
+
+void view_diagrams_custom(
+			char *application_name,
+			boolean all_documents,
+			char *login_name,
+			char *relative_directory,
+			char *document_root_directory,
+			char *appaserver_mount_point );
 
 int main( int argc, char **argv )
 {
 	char *application_name;
-	char current_directory[ 256 ];
 	DOCUMENT *document;
-	LIST *file_list;
-	char *file_to_process;
-	char buffer[ 512 ];
 	APPASERVER_PARAMETER_FILE *appaserver_parameter_file;
 	char *login_name = "";
 	char *database_string = {0};
-	char base_name[ 128 ];
 	boolean all_documents = 0;
 	char *relative_directory = {0};
 	char *process_name = {0};
@@ -54,7 +60,7 @@ int main( int argc, char **argv )
 	if ( argc < 2 )
 	{
 		fprintf( stderr,
-	"Usage: %s application [all_documents_yn] [login_name] [role] [relative_directory] [process_name]\n",
+	"Usage: %s application [all_documents_yn] [login_name] [unused] [relative_directory] [process_name]\n",
 			 argv[ 0 ] );
 		exit ( 1 );
 	}
@@ -83,18 +89,101 @@ int main( int argc, char **argv )
 	appaserver_parameter_file = appaserver_parameter_file_new();
 	add_pwd_to_path();
 
+	document = document_new( "", application_name );
+
+	document_output_head(
+			document->application_name,
+			document->title,
+			document->output_content_type,
+			appaserver_parameter_file->appaserver_mount_point,
+			document->javascript_module_list,
+			"diagram_list.css",
+			application_get_relative_source_directory(
+				application_name ),
+			0 /* not with_dynarch_menu */ );
+
+	document_output_body(
+			document->application_name,
+			document->onload_control_string );
+
+	if ( process_name )
+	{
+		char buffer[ 128 ];
+
+		printf(	"<h1>%s</h1>\n",
+			format_initial_capital(
+				buffer,
+				process_name ) );
+	}
+	else
+	{
+		printf( "<h1>Documentation List</h1>\n" );
+	}
+
+	if ( argc == 2 )
+	{
+		view_diagrams_generic(
+			application_name,
+			appaserver_parameter_file->appaserver_mount_point );
+	}
+	else
+	{
+		view_diagrams_custom(
+			application_name,
+			all_documents,
+			login_name,
+			relative_directory,
+			appaserver_parameter_file->document_root,
+			appaserver_parameter_file->appaserver_mount_point );
+	}
+
+	document_close();
+
+	if ( process_name )
+	{
+		process_increment_execution_count(
+			application_name,
+			process_name,
+			appaserver_parameter_file_get_dbms() );
+	}
+	else
+	{
+		process_increment_execution_count(
+			application_name,
+			PROCESS_NAME,
+			appaserver_parameter_file_get_dbms() );
+	}
+
+	exit( 0 );
+
+} /* main() */
+
+void view_diagrams_custom(
+			char *application_name,
+			boolean all_documents,
+			char *login_name,
+			char *relative_directory,
+			char *document_root_directory,
+			char *appaserver_mount_point )
+{
+	char current_directory[ 256 ];
+	LIST *file_list;
+	char *file_to_process;
+	char base_name[ 128 ];
+	char buffer[ 512 ];
+
 	if ( relative_directory )
 	{
 		sprintf(current_directory,
 			"%s/%s",
-		 	appaserver_parameter_file->document_root,
+		 	document_root_directory,
 			relative_directory );
 	}
 	else
 	{
 		sprintf(current_directory,
 		 	"%s/%s", 
-		 	appaserver_parameter_file->appaserver_mount_point,
+		 	appaserver_mount_point,
 		 	application_get_first_relative_source_directory(
 				application_name ) );
 	}
@@ -112,50 +201,11 @@ int main( int argc, char **argv )
 	else
 		file_list = pipe2list( SYS_STRING );
 
-	document = document_new( "", application_name );
-
-/*
-	if ( !*login_name
-	||   !appaserver_get_frameset_menu_horizontal(
-					application_name,
-					login_name ) )
-	{
-		document_set_output_content_type( document );
-	}
-*/
-
-	document_output_head(
-			document->application_name,
-			document->title,
-			document->output_content_type,
-			appaserver_parameter_file->appaserver_mount_point,
-			document->javascript_module_list,
-			"diagram_list.css",
-			application_get_relative_source_directory(
-				application_name ),
-			0 /* not with_dynarch_menu */ );
-
-	document_output_body(
-			document->application_name,
-			document->onload_control_string );
-
 	if ( !list_rewind( file_list ) )
 	{
 		printf( "<p>ERROR: no documents found\n" );
 		document_close();
 		exit( 1 );
-	}
-
-	if ( process_name )
-	{
-		printf(	"<h1>%s</h1>\n",
-			format_initial_capital(
-				buffer,
-				process_name ) );
-	}
-	else
-	{
-		printf( "<h1>Documentation List</h1>\n" );
 	}
 
 	printf( "<ul>\n" );
@@ -198,24 +248,71 @@ int main( int argc, char **argv )
 
 	if ( *login_name ) form_output_back_to_top_button();
 
-	document_close();
+} /* view_diagrams_custom() */
 
-	if ( process_name )
+void view_diagrams_generic(
+			char *application_name,
+			char *appaserver_mount_point )
+{
+	char current_directory[ 256 ];
+	LIST *file_list;
+	char *file_to_process;
+	char base_name[ 128 ];
+	char buffer[ 512 ];
+	char *relative_source_directory_list_string;
+	char relative_source_directory[ 128 ];
+	int p;
+
+	relative_source_directory_list_string =
+		application_get_relative_source_directory(
+			application_name );
+
+	printf( "<ul>\n" );
+
+	for(	p = 0;
+		piece(	relative_source_directory,
+			':',
+			relative_source_directory_list_string,
+			p );
+		p++ )
 	{
-		process_increment_execution_count(
-			application_name,
-			process_name,
-			appaserver_parameter_file_get_dbms() );
+		sprintf(current_directory,
+		 	"%s/%s", 
+		 	appaserver_mount_point,
+			relative_source_directory );
+
+		if ( chdir( current_directory ) == -1 )
+		{
+			fprintf(stderr,
+			 	"Cannot change directory to %s\n",
+			 	current_directory );
+			continue;
+		}
+
+		file_list = pipe2list( SYS_STRING );
+
+		if ( !list_rewind( file_list ) ) continue;
+
+		do {
+			file_to_process = list_get_string( file_list );
+
+			strcpy( base_name,
+				basename_get_base_name(
+					file_to_process,
+					1 /* strip_extension */ ) );
+
+			printf(
+"<li><a class=diagram target=\"%s\" href=\"/appaserver/%s/%s\">%s</a>\n",
+				base_name,
+				relative_source_directory,
+		 		file_to_process,
+				format_initial_capital(
+					buffer,
+					file_to_process ) );
+
+		} while( list_next( file_list ) );
 	}
-	else
-	{
-		process_increment_execution_count(
-			application_name,
-			PROCESS_NAME,
-			appaserver_parameter_file_get_dbms() );
-	}
 
-	exit( 0 );
+	printf( "</ul>\n" );
 
-} /* main() */
-
+} /* view_diagrams_generic() */

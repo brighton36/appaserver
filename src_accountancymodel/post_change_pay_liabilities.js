3d29f7fed6366b7786dc53fde771ceb929597f4f
Tim Riley <timriley@appahost.com>
2017-11-29 15:40:28 -0800

    made dialog box intelligent

--- src_accountancymodel/post_change_pay_liabilities.js
+++ src_accountancymodel/post_change_pay_liabilities.js
@@ -1,38 +1,30 @@
-// src_accountancymodel/post_change_post_reoccurring_transaction.js
-// ----------------------------------------------------------------
+// src_accountancymodel/post_change_pay_liabilities.js
+// ---------------------------------------------------
 
-function post_change_post_reoccurring_transaction( current_element )
+function post_change_pay_liabilities( current_element )
 {
 	var element;
-	var entity_street_address_split;
-	var entity_street_address;
-	var options;
+	var value;
 
-	element_name = 'entity_name^street_address';
+	element = timlib_get_element( "memo" );
+	element.disabled = 1;
 
-	element = timlib_get_element( "entity_name^street_address_0" );
+	element = timlib_get_element( "with_stub_yn" );
+	element.disabled = 1;
 
-	value = timlib_get_drop_down_element_value( element.options );
+	element = timlib_get_element( "starting_check_number" );
+	value = element.value;
 
-	if ( value == 'undefined' || value == 'select' )
-		return true;
+	if ( value >= 1 )
+	{
+		element = timlib_get_element( "memo" );
+		element.disabled = 0;
 
-	entity_street_address_split = value.split( '^' );
-	entity_street_address = entity_street_address_split[ 1 ];
+		element = timlib_get_element( "with_stub_yn" );
+		element.disabled = 0;
+	}
 
-	element = timlib_get_element( "rental_property_street_address_0" );
-	element.disabled = 0;
-	options = element.options;
-
-        for( var i = 0; i < options.length; i++ )
-        {
-                if ( options[ i ].value == entity_street_address )
-                {
-			element.disabled = 1;
-			timlib_set_drop_down_element_value( options, "Select" )
-                }
-        }
 	return true;
 
-} // post_change_post_reoccurring_transaction()
+} // post_change_pay_liabilities()
 

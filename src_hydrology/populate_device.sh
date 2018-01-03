9f989938ee9295cdc847416f0090b3de653e2b72
Tim Riley <timriley@appahost.com>
2018-01-03 11:52:44 -0800

    added where clause parameter

--- src_hydrology/populate_device.sh
+++ src_hydrology/populate_device.sh
@@ -2,30 +2,40 @@
 
 echo Starting: $0 $* 1>&2
 
-if [ "$#" -ne 3 ]
+if [ "$#" -ne 4 ]
 then
-	echo "Usage: $0 application one2m_folder station" 1>&2
+	echo "Usage: $0 application one2m_folder station where" 1>&2
 	exit 1
 fi
 
 application=$1
 one2m_folder=$2
 station=$3
+parameter_where=$4
+
+if [ "$parameter_where" = "where" ]
+then
+	preprompt_where="1 = 1"
+else
+	preprompt_where="$parameter_where"
+fi
 
 select="device_type,manufacturer,model,serial_number"
 
 if [ "$one2m_folder" = "site_visit_device_addition" ]
 then
-	where="current_station is null"
+	one2m_where="current_station is null"
 elif [ "$one2m_folder" = "site_visit_device_removal" ]
 then
-	where="current_station = '$station'"
+	one2m_where="current_station = '$station'"
 elif [ "$one2m_folder" = "device_repair" ]
 then
-	where="current_station is null"
+	one2m_where="current_station is null"
 else
-	where="1 = 1"
+	one2m_where="1 = 1"
 fi
+
+where="$parameter_where and $one2m_where"
 
 echo "select $select from device where $where order by $select;"	|
 sql.e

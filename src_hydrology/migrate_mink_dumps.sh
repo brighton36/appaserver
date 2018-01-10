9cc20738c300f40635bf0492875ac5285faf6404
Tim Riley <timriley@appahost.com>
2018-01-10 09:27:38 -0800

    rsync doesn't preserve links

--- src_hydrology/migrate_mink_dumps.sh
+++ src_hydrology/migrate_mink_dumps.sh
@@ -2,7 +2,16 @@
 # migrate_mink_dumps.sh
 # ---------------------
 
+block_tar=backup_block.tar
+line_tar=backup_line.tar
+sparrow_tar=sparrow.tar
+
 cd /opt/physical/data/dfe
-nohup /usr/bin/time rsync -a --progress --delete ./backup_block ./backup_line ./sparrow bonefish:/opt/physical/data/dfe >/var/tmp/migrate_mink_dumps.out 2>&1 &
+
+tar cvf $block_tar backup_block
+tar cvf $line_tar backup_line
+tar cvf $sparrow_tar sparrow
+
+nohup /usr/bin/time rsync -a --progress $block_tar $line_tar $sparrow_tar bonefish:/opt/physical/data/dfe >/var/tmp/migrate_mink_dumps.out 2>&1 &
 
 exit 0

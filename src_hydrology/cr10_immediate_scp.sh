c7160c1d72c87b96e58286c80c323f556e29c41d
Tim Riley <timriley@appahost.com>
2018-01-16 16:14:22 -0500

    having mink's cr10 files be automatically copied to bonefish

--- src_hydrology/cr10_immediate_scp.sh
+++ src_hydrology/cr10_immediate_scp.sh
@@ -6,6 +6,10 @@
 # Freely available software: see Appaserver.org
 # ---------------------------------------------
 
+# Place this in mink:/etc/rc.local
+# --------------------------------
+#su - timriley -c "nohup /opt/physical/appaserver/utility/inotifywait.sh /var/export/enpdata cr10_immediate_scp.sh >/tmp/inotifywait.out 2>&1 &"
+
 if [ "$#" -ne 1 ]
 then
 	echo "Usage: $0 cr10file" 1>&2

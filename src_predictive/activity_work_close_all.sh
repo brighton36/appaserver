32c6c2dc104b095b64dd16c76867e96fda850809
Tim Riley <timriley@appahost.com>
2018-02-09 12:18:22 -0800

    implementing activity based costing

---
+++ src_predictive/activity_work_close_all.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# ----------------------------------------------------------
+# $APPASERVER_HOME/src_predictive/activity_work_close_all.sh
+# ----------------------------------------------------------
+
+if [ "$#" -ne 3 ]
+then
+	echo "Usage: $0 application login_name begin_work_date_time" 1>&2
+	exit 1
+fi
+
+application=$(echo $1 | piece.e ':' 0)
+database=$(echo $1 | piece.e ':' 1 2>/dev/null)
+
+if [ "$database" != "" ]
+then
+	export DATABASE=$database
+else
+	export DATABASE=$application
+fi
+
+login_name=$2
+begin_work_date_time=$3
+
+now=`now.sh colon | piece_inverse.e 2 ':'`
+
+function activity_work_close()
+{
+	application=$1
+	login_name=$2
+	begin_work_date_time=$3
+	now=$4
+
+	table="activity_work"
+
+	key="		login_name,
+			begin_work_date_time"
+
+	select="	login_name,
+			substr( begin_work_date_time, 1, 16 )"
+
+	where="		login_name = '${login_name}' and
+			begin_work_date_time <> '$begin_work_date_time' and
+			end_work_date_time is null"
+
+	echo "select ${select} from $table where $where;"		|
+	sql.e								|
+	while read record
+	do
+		echo "${record}^end_work_date_time^${now}"		|
+		update_statement.e table=$table key="$key" carrot=y	|
+		sql.e
+
+		login_name=`echo $record | piece.e '^' 0`
+		begin_work_date_time=`echo $record | piece.e '^' 1`
+
+		post_change_activity_work		\
+			"${application}"		\
+			"${login_name}"			\
+			"${begin_work_date_time}"	\
+			update
+	done
+}
+
+activity_work_close "$application" "$login_name" "$begin_work_date_time" "$now"
+
+exit 0

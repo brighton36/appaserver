0dcc910c9b11240c156e40084a9d8c7446f8f4ea
Tim Riley <timriley@appahost.com>
2018-04-13 08:48:54 -0700

    added magic map libraries

--- src_hydrology/migrate_bonefish_python.sh
+++ src_hydrology/migrate_bonefish_python.sh
@@ -5,6 +5,10 @@
 # Run this after migrate_mink.sh
 # ------------------------------
 
+sudo apt-get -y install python3-mpltoolkits.basemap
+sudo apt-get -y install python3-pandas
+sudo apt-get -y install python3-geopandas
+
 cd /dfe/tmp
 subtract_list.e python_mink.dat python_bonefish.dat	|
 xargs.e sudo apt-get -y install {}

10d745c7c821e36ca0e503b842dd0996a8f662c1
Tim Riley <timriley@appahost.com>
2018-05-04 10:35:45 -0700

    allowing in_clause to accept argv[1]

--- utility/in_clause.c
+++ utility/in_clause.c
@@ -10,18 +10,41 @@
 #include "list.h"
 #include "timlib.h"
 
-int main( void )
+void output_in_clause_stdin( void );
+void output_in_clause_parameter( char *argv_1 );
+
+int main( int argc, char **argv )
+{
+	if ( argc == 2 )
+	{
+		output_in_clause_parameter( argv[ 1 ] );
+	}
+	else
+	{
+		output_in_clause_stdin();
+	}
+
+	return 0;
+
+} /* main() */
+
+void output_in_clause_stdin( void )
 {
 	char input_buffer[ 1024 ];
 	LIST *data_list;
-
 	data_list = list_new();
 
 	while( get_line( input_buffer, stdin ) )
-		list_append_pointer( data_list, strdup( input_buffer ) );
+		list_append_pointer(
+			data_list,
+			strdup( input_buffer ) );
 
 	printf( "in (%s)\n", timlib_with_list_get_in_clause( data_list ) );
 
-	return 0;
-} /* main() */
+} /* output_in_clause_stdin() */
 
+void output_in_clause_parameter( char *argv_1 )
+{
+	printf( "in (%s)\n", timlib_get_in_clause( argv_1 ) );
+
+} /* output_in_clause_parameter() */

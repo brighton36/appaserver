42aba64d5335ab904d2a69e74c1fd6562c35a152
Tim Riley <timriley@appahost.com>
2018-05-15 17:31:24 -0700

    made FOLDER_ATTRIBUTE primary_key_index and display_order mutually exclusive

---
+++ javascript/post_change_folder_attribute.js
@@ -0,0 +1,80 @@
+// $APPASERVER_HOME/javascript/post_change_folder_attribute.js
+// -----------------------------------------------------------
+function post_change_folder_attribute( row )
+{
+	var element_name;
+	var element;
+	var primary_key_element_value;
+	var display_order_element_value;
+
+	if ( row == 0 )
+	{
+		post_change_folder_attribute_each_row();
+		// return true; <-- Need to pass through for insert screen.
+	}
+
+	// Turn on each edit.
+	// ------------------
+	element_name = 'primary_key_index_' + row;
+	element = timlib_get_element( element_name );
+
+	if ( element == "" ) return false;
+
+	element.disabled = false;
+
+	element_name = 'display_order_' + row;
+	element =
+		timlib_get_element( element_name );
+
+	if ( element == "" ) return false;
+
+	element.disabled = false;
+
+	// Turn off one of them
+	// --------------------
+	element_name = 'primary_key_index_' + row;
+	element = timlib_get_element( element_name );
+
+	primary_key_element_value = Number( element.value );
+
+	element_name = 'display_order_' + row;
+	element = timlib_get_element( element_name );
+
+	display_order_element_value = Number( element.value );
+
+	if ( primary_key_element_value )
+	{
+		element_name = 'display_order_' + row;
+		element = timlib_get_element( element_name );
+
+		element.disabled = true;
+	}
+	else
+	if ( display_order_element_value )
+	{
+		element_name = 'primary_key_index_' + row;
+		element = timlib_get_element( element_name );
+
+		element.disabled = true;
+	}
+
+	return true;
+
+} // post_change_folder_attribute()
+
+function post_change_folder_attribute_each_row()
+{
+	var row;
+	var results;
+
+	row = 1;
+	while( true )
+	{
+		results = post_change_folder_attribute( row );
+		if ( results == false ) break;
+		row++;
+	}
+
+	return true;
+
+} // post_change_folder_attribute_each_row()

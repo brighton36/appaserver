2f5b87c533e8d426ac3819a23b4e47c6c88f4d08
Tim Riley <timriley@appahost.com>
2019-05-15 18:06:03 +0000

    new script table_change_email.sh

---
+++ src_appaserver/table_change_email.sh
@@ -0,0 +1,35 @@
+:
+# $APPASERVER_HOME/src_appaserver/table_change_email.sh
+# -----------------------------------------------------
+
+if [ "$APPASERVER_DATABASE" != "" ]
+then
+	application=$APPASERVER_DATABASE
+elif [ "$DATABASE" != "" ]
+then
+	application=$DATABASE
+fi
+
+if [ "$application" = "" ]
+then
+	echo "Error in `basename.e $0 n`: you must first:" 1>&2
+	echo "\$ . set_database" 1>&2
+	exit 1
+fi
+
+if [ "$#" -ne 2 ]
+then
+	echo "Usage: $0 table email" 1>&2
+	exit 1
+fi
+
+table=$1
+email=$2
+
+change=`table_count_change.sh $table | grep "Change:" | piece.e ':' 1`
+
+message="$table count change: $change"
+
+echo "$message" | mailx -s "$message" $email
+
+exit 0
